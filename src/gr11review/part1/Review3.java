48b4c0350b8b7a38f7495cb0ed5424568e18e4d1
Ashley Fong <>
2021-04-28 19:12:04 +0000

    review 3

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,30 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/**
+* 
+* @author: Ashley Fong
+* 
+*/
+
+public class Review3 { 
+  public static void main(String[] args) throws IOException{
+		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+    
+    int i; 
+    
+    for(i = 21; i <= 100; i = i + 2){
+      System.out.println(i);
+
+    }
+
+    System.out.println("");
+
+    for(i = 29; i >= 2; i--){
+      System.out.println(i);
+
+    }
+    
+  }
+}
