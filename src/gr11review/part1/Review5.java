f6527364529354927e974430be820498936e30e9
Ashley Fong <>
2021-04-30 18:12:43 +0000

    5 comments

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -12,13 +12,15 @@ public class Review5 {
   public static void main(String[] args) throws IOException{
 		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+    //variables
     double yearlyAmount; 
     double compInterestRate; 
     double compAmount;
     double targetAmount; 
     double totalAmount = 0;
     int numYears = 0; 
-
+    
+    //ask user for input of invested amount, interest rate, and target amount
     System.out.print("Enter the yearly invested amount: ");
     yearlyAmount = Double.parseDouble(key.readLine());
 
@@ -28,13 +30,15 @@ public class Review5 {
     System.out.print("Enter the target amount: ");
     targetAmount = Double.parseDouble(key.readLine());
 
-    while(targetAmount > totalAmount){
+    //loops until the total amount is passes the target amount
+    while (targetAmount > totalAmount) {
       compAmount = totalAmount + yearlyAmount; 
       totalAmount = (compInterestRate/100 * compAmount);
       totalAmount = compAmount + totalAmount;
       numYears++;
     }
     
+    //prints the number of years it takes to earn the target amount 
     System.out.println("The target amount will be earned in "+numYears+" years.");
 
   }
