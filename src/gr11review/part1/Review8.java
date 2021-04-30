ce90faa69863838b5e007e82f824d699ff98cf2d
Ashley Fong <>
2021-04-30 04:06:38 +0000

    review8 comments

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -12,17 +12,19 @@ public class Review8 {
   public static void main(String[] args) throws IOException{
 		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+    //variables 
     int i; 
     int c; 
     int numTriples = 0; 
     int random;
     
+    //set up array
     int slotMach[][];
     slotMach = new int[1000][3];
 
+    //randomizing slot numbers and loading them in the array 
     for (i = 0; i < 1000; i++){
       for(c = 0; c < 3; c++){
-        //System.out.println(i);
         random = (int)(Math.random()*9);
         slotMach[i][c] = random;
         System.out.print(slotMach[i][c]+" ");
@@ -30,6 +32,8 @@ public class Review8 {
 
       System.out.println("");
 
+      //checking if all the numbers in a pull are the simulates
+      //if yes, the number of triples increases
       if(slotMach[i][0] == slotMach[i][1] && slotMach[i][1] == slotMach[i][2]){
         numTriples++;
       }
