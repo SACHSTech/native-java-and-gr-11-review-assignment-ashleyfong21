c17a17e5810f815c207f4712afe40e979ce1765e
Ashley Fong <>
2021-04-30 17:54:30 +0000

    review 6 comments

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -14,22 +14,27 @@ public class Review6 {
     DecimalFormat decimalFormat = new DecimalFormat("0.00");
 		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+    //variables
     int i = 0; 
     double itemPrice = 1; 
     double subtotal = 0;
     double tax;
     double total; 
 
+    //loops while the input is not 0
+    //add the prive of the item to the subtotal 
     while(itemPrice != 0){
         i++;
-        System.out.print("Enter the price for item "+i+": ");
+        System.out.print("Enter the price for an item: ");
         itemPrice = Double.parseDouble(key.readLine());
         subtotal = subtotal + itemPrice;
     }
 
+    //calculates the tax and total
     tax = subtotal * 0.13;
     total = subtotal + tax; 
 
+    //prints the subtotal, tax, and total in decimal format
     System.out.println("Subtotal: $"+ decimalFormat.format(subtotal));
     System.out.println("Tax: $"+ decimalFormat.format(tax));
     System.out.println("Total: $"+ decimalFormat.format(total));
