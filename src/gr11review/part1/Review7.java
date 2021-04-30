7e3c099b0a03c1322f7ed350c7e687ddc4599e2c
Ashley Fong <>
2021-04-30 02:56:18 +0000

    review 8

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,26 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/**
+* A program that asks the user for a yearly amount, annual compound interest rate, and target amount, and outputs the number of years it will take to surpass that amount. For compound interest, the interest rate is applied to the existing amount after each year.                                                                    
+* @author: Ashley Fong
+* 
+*/
+
+public class Review5 { 
+  public static void main(String[] args) throws IOException{
+		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    String theSentence; 
+    int numChar; 
+    int numSpaces;
+    int letterA;
+    int oddChar; 
+
+    System.out.println("Enter a sentence: ");
+    theSentence = key.readLine();
+    System.out.println("")
+
+  }
+}
--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,47 @@
 package gr11review.part1;
+
+import java.io.*;
+
+/**
+*                                                                    
+* @author: Ashley Fong
+* 
+*/
+
+//for 1000 loop 
+
+//for 3 loop 
+
+//if all the #s are equal, triples++ 
+
+public class Review8 { 
+  public static void main(String[] args) throws IOException{
+		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    int i; 
+    int c; 
+    int numTriples = 0; 
+    int random;
+    
+    int slotMach[][];
+    slotMach = new int[1000][3];
+
+    for (i = 0; i < 1000; i++){
+      for(c = 0; c < 3; c++){
+        //System.out.println(i);
+        random = (int)(Math.random()*9);
+        slotMach[i][c] = random;
+        System.out.print(slotMach[i][c]+" ");
+      }
+
+      System.out.println("");
+
+      if(slotMach[i][0] == slotMach[i][1] && slotMach[i][1] == slotMach[i][2]){
+        numTriples++;
+      }
+    }
+
+    System.out.println(numTriples);
+
+  }
+}
