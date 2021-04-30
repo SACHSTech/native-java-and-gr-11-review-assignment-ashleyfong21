23d8b183f56fc74a0d646beb6e31ed4aa46171db
Ashley Fong <>
2021-04-30 20:56:39 +0000

    comments

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -12,72 +12,75 @@ public class Review1 {
   public static void main(String[] args) throws IOException{
 		BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+    //variables
     int monthNum;
     int dayNum;
     int dayYear;
 
+    //user inputs of month and day numbers 
     System.out.print("Enter the month number: ");
 		monthNum = Integer.parseInt(key.readLine());
 
     System.out.print("Enter the day number: ");
     dayNum = Integer.parseInt(key.readLine());
 
-    if(monthNum == 1){
+    //prints the day of the year based on the month that is picked
+    if (monthNum == 1) {
       dayYear = 0 + dayNum; 
       System.out.println(dayYear); 
     }
 
-    else if(monthNum == 2){
+    else if (monthNum == 2) {
       dayYear = 31 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 3){
+    else if (monthNum == 3) {
       dayYear = 31 + 28 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 4){
+    else if (monthNum == 4) {
       dayYear = 31 + 28 + 31 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 5){
+    else if (monthNum == 5) {
       dayYear = 31 + 28 + 31 + 30 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 6){
+    else if (monthNum == 6) {
       dayYear = 31 + 28 + 31 + 30 + 31 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 7){
+    else if (monthNum == 7) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 8){
+    else if (monthNum == 8) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + 31 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 9){
+    else if (monthNum == 9) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 30 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 10){
+    else if (monthNum == 10) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 30 + 31 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 11){
+    else if (monthNum == 11) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 30 + 31 + 30 + dayNum;
       System.out.println(dayYear);
     }
 
-    else if(monthNum == 12){
+    else if (monthNum == 12) {
       dayYear = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 30 + 31 + 30 + 31 + dayNum;
       System.out.println(dayYear);
     }
